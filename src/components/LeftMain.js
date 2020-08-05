bc6fa03f7eec59e6bf27070704cbd2036ab56412
unknown <KalebJohnson45@yahoo.com>
2020-08-05 16:14:08 -0500

    Icon buttons

--- src/components/LeftMain.js
+++ src/components/LeftMain.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TimeLine from './TimeLine';
-import { Button } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import Fade from 'react-reveal/Fade';
 import Flash from 'react-reveal/Flash';
@@ -21,7 +21,7 @@ return(
                 <h2 style={{fontSize:'3rem'}}>A CREATIVE ADVENTURE TOWARDS MARKETING</h2>
                 <p style={{ border:"1px solid white", padding:'1rem'}}>Simple, Lovely, and everything in between.</p>
                     <Flash duration="3000">
-                    <Button style={{color:'white'}} onClick={() => { scrollWin(0, 1050) }}><ArrowDownwardIcon style={{fontSize:'5rem', margin:'1rem'}}/></Button>
+                    <IconButton style={{color:'white'}} onClick={() => { scrollWin(0, 1050) }}><ArrowDownwardIcon style={{fontSize:'5rem', margin:'1rem'}}/></IconButton>
                     </Flash>
             </Fade>
         </div>
